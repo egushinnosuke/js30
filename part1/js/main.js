b876f69155b6e11491080e7dd97d7625405d0051
egushinnosuke <mail@office-eguchi.co.jp>
2019-09-13 17:00:04 +0900

    test

--- part1/js/main.js
+++ part1/js/main.js
@@ -1,3 +1,9 @@
 window.addEventListener("keydown", function(e){
     console.log(e.keyCode);
+
+    
 });
+
+function changeStyle(key){
+    document.querySelector('.key[data-keycord]='+key).style.color = white;
+}
