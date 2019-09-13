31ef021b1d3613c9f8fdecfdc34169951a8a851b
egushinnosuke <mail@office-eguchi.co.jp>
2019-09-13 17:20:25 +0900

    add audio play

--- part1/js/main.js
+++ part1/js/main.js
@@ -1,15 +1,18 @@
 window.addEventListener("keydown", function(e){
     console.log(e.keyCode);
     changeStyle(e.keyCode)
-    
+    playSound(e.keyCode)
 });
 
 function changeStyle(key){
-    let key_elm = document.querySelector('.key[data-keycord="'+key+'"]');
-    key_elm.style.color = "white";
+    let key_elm = document.querySelector('.key[data-keycode="'+key+'"]');
+    key_elm.classList.add("now-playing");
+    setTimeout(function(){
+        key_elm.classList.remove("now-playing")
+    }, 500);
 }
 
-function sound(key){
-    let sound_elm = document.querySelector('audio[data-keycord="'+key+'"]');
+function playSound(key){
+    let sound_elm = document.querySelector('audio[data-keycode="'+key+'"]');
     sound_elm.play();
 }
